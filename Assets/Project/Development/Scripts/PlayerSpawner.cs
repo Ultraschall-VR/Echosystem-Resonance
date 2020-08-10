a1d5ae1995a4831898e7a62d4de382cfb779cbc3
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2020-08-10 16:42:40 +0200

    Fixed issues:
    Fixed elevation problem with stairs;
    Player now instantiates on start;

---
+++ Assets/Project/Development/Scripts/PlayerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private Transform _playerSpawn;
+
+    [HideInInspector] public GameObject PlayerInstance = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInstance = Instantiate(_playerPrefab, _playerSpawn.position, _playerSpawn.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
