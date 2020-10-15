5b39e55658e83051d2fdc97c026977753f455204
Alexej <alexej.wieber@ultraschall-vr.com>
2020-10-15 15:35:13 +0200

    01_OceanFloor Audio

---
+++ Assets/Audio/Scripts/AudioDelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioDelay : MonoBehaviour
+{
+    public float DelayTime=0;
+    AudioSource audioData;
+
+    void Start() {
+        audioData = GetComponent<AudioSource>();
+        audioData.PlayDelayed(DelayTime);
+        Debug.Log("started");
+    }
+}
---
+++ Assets/Audio/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    float timeCounter = 0;
+
+    public float speed=1;
+    public float width=2;
+    //public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(0, 0, 0);
+        timeCounter += Time.deltaTime*speed;
+
+        float x = Mathf.Cos (timeCounter)*width;
+        float y = 1;
+        float z = Mathf.Sin (timeCounter)*width;
+
+        transform.position = transform.localPosition + new Vector3(x, y, z);
+    }
+}
