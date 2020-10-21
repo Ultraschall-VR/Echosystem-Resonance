3900b74c46b6017c72aceb17e928f85c10b27c84
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.de>
2020-10-21 17:35:57 +0200

    Added MainMenu

---
+++ Assets/MenuElement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class MenuElement : MonoBehaviour
+{ 
+    private TextMeshProUGUI _text;
+    [SerializeField] private Color _highlightColor;
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _selectColor;
+    
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _text.faceColor = _defaultColor;
+    }
+
+    public void Highlight()
+    {
+        _text.faceColor = _highlightColor;
+        //StartCoroutine(ChangeState(_defaultColor, _highlightColor));
+    }
+
+    public void DeHighlight()
+    {
+        _text.faceColor = _defaultColor;
+        //StartCoroutine(ChangeState(_highlightColor, _defaultColor));
+    }
+
+    public void Select()
+    {
+        _text.faceColor = _selectColor;
+        //StartCoroutine(ChangeState(_highlightColor, _selectColor));
+    }
+
+    private IEnumerator ChangeState(Color from, Color to)
+    {
+        float t = 0;
+        float timer = 0.5f;
+
+        while (t <= timer)
+        {
+            t += Time.deltaTime;
+            _text.faceColor = Color.Lerp(from, to, t);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/UiInteraction.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class UiInteraction : MonoBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private LineRendererCaster _lineRendererCaster;
+
+    private MenuElement _selection = null;
+
+    private void CalculateRaycast()
+    {
+        if (_selection != null)
+        {
+            _selection.DeHighlight();
+        }
+
+        RaycastHit hit;
+        _lineRendererCaster.Hide();
+
+        if (Physics.Raycast(_playerInput.ControllerRight.transform.position,
+            _playerInput.ControllerRight.transform.forward, out hit,
+            Mathf.Infinity))
+        {
+            if (hit.transform.CompareTag("UI"))
+            {
+                _lineRendererCaster.ShowUiRaycast(_playerInput.ControllerRight.transform.position, hit.point, 16);
+
+                MenuElement menuElement = hit.transform.GetComponent<MenuElement>();
+                
+                menuElement.Highlight();
+
+                if (_playerInput.RightTriggerPressed.state)
+                {
+                    menuElement.Select();
+                }
+
+                _selection = menuElement;
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        CalculateRaycast();
+    }
+}
