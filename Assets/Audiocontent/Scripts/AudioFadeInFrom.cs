edb0e3431943ee9378715485728ea69a2ecb89b8
Alexej <alexej.wieber@ultraschall-vr.com>
2020-11-04 18:10:14 +0100

    Optimized Mixer Structure

---
+++ Assets/Audiocontent/Scripts/AudioFadeInFrom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+
+public class AudioFadeInFrom : MonoBehaviour {
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] string exposedParameter;
+    [SerializeField] float duration = 10, targetVolume = 1, fromVolume = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(FadeMixerGroupFrom.StartFadeFrom(audioMixer, exposedParameter, duration, fromVolume, targetVolume));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
