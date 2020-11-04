d7d94fb3b473be242d0b5f94c2fca7b4fb55043b
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.de>
2020-11-04 19:20:38 +0100

    Worked on GroundFloor

--- Assets/LevelDesignSpawner.cs
+++ Assets/LevelDesignSpawner.cs
@@ -6,7 +6,11 @@ using Random = UnityEngine.Random;
 
 public class LevelDesignSpawner : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _prefabs;
+    [SerializeField] private List<GameObject> _randomRotPrefabs;
+    [SerializeField] private int _randomRotSpawnCount;
+    
+    [SerializeField] private List<GameObject> _randomYRotPrefabs;
+    [SerializeField] private int _randomYRotSpawnCount;
 
     [SerializeField] private int _sizeX;
     [SerializeField] private int _sizeZ;
@@ -19,30 +23,67 @@ public class LevelDesignSpawner : MonoBehaviour
     [ContextMenu("SpawnObjects")]
     public void SpawnObjects()
     {
-        Clear();
-        
-        for (int i = 0; i < _spawnCount; i++)
+        SpawnRotRandom();
+        SpawnYRotRandom();
+    }
+
+    private void SpawnYRotRandom()
+    {
+        for (int i = 0; i < _randomYRotSpawnCount; i++)
         {
-            var randObject = Random.Range(0, _prefabs.Count);
-            
+            var randObject = Random.Range(0, _randomYRotPrefabs.Count);
+
             var randPosX = Random.Range(transform.position.x, transform.position.x + _sizeX);
-            var randPosZ =  Random.Range(transform.position.z, transform.position.z + _sizeZ);
-            
-            var randRot = new Vector3(Random.Range(0,360), Random.Range(0,360), Random.Range(0,360));
-            
+            var randPosZ = Random.Range(transform.position.z, transform.position.z + _sizeZ);
+
+            var randRot = new Vector3(0, Random.Range(0, 360), 0);
+
             var randPos = new Vector3(randPosX, 100, randPosZ);
-            
-            var instance = Instantiate(_prefabs[randObject], randPos, Quaternion.Euler(randRot));
+
+            var instance = Instantiate(_randomYRotPrefabs[randObject], randPos, Quaternion.Euler(randRot));
 
             RaycastHit hit;
-            
-            if(Physics.Raycast(instance.transform.position, Vector3.down, out hit))
+
+            if (Physics.Raycast(instance.transform.position, Vector3.down, out hit))
 
                 if (hit.collider.CompareTag("TeleportArea"))
                 {
                     instance.transform.position = hit.point;
                     instance.transform.SetParent(this.transform);
-                    
+
+                    _instances.Add(instance);
+                }
+                else
+                {
+                    DestroyImmediate(instance.gameObject);
+                }
+        }
+    }
+
+    private void SpawnRotRandom()
+    {
+        for (int i = 0; i < _randomRotSpawnCount; i++)
+        {
+            var randObject = Random.Range(0, _randomRotPrefabs.Count);
+
+            var randPosX = Random.Range(transform.position.x, transform.position.x + _sizeX);
+            var randPosZ = Random.Range(transform.position.z, transform.position.z + _sizeZ);
+
+            var randRot = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+
+            var randPos = new Vector3(randPosX, 100, randPosZ);
+
+            var instance = Instantiate(_randomRotPrefabs[randObject], randPos, Quaternion.Euler(randRot));
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(instance.transform.position, Vector3.down, out hit))
+
+                if (hit.collider.CompareTag("TeleportArea"))
+                {
+                    instance.transform.position = hit.point;
+                    instance.transform.SetParent(this.transform);
+
                     _instances.Add(instance);
                 }
                 else
@@ -60,7 +101,10 @@ public class LevelDesignSpawner : MonoBehaviour
             foreach (var instance in _instances)
             {
                 DestroyImmediate(instance);
+                
             }
+            
+            _instances = new List<GameObject>();
         }
     }
 }
