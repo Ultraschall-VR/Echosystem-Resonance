e3d6748329cd058fa15e353eff18b5a47b679594
Alexej <alexej.wieber@ultraschall-vr.com>
2020-11-07 18:36:41 +0100

    Finished Audio 'LoadingScene_OrpheusToOceanFloor'

--- Assets/Audiocontent/Scripts/AudioFadeInFrom.cs
+++ Assets/Audiocontent/Scripts/AudioFadeInFrom.cs
@@ -8,11 +8,11 @@ using UnityEngine.Audio;
 public class AudioFadeInFrom : MonoBehaviour {
     [SerializeField] AudioMixer audioMixer;
     [SerializeField] string exposedParameter;
-    [SerializeField] float duration = 10, targetVolume = 1, fromVolume = 0;
+    [SerializeField] float duration = 10, targetVolume = 1, fromVolume = 0, delay = 0;
 
     // Start is called before the first frame update
     void Start() {
-        StartCoroutine(FadeMixerGroupFrom.StartFadeFrom(audioMixer, exposedParameter, duration, fromVolume, targetVolume));
+        StartCoroutine(FadeMixerGroupFrom.StartFadeFrom(audioMixer, exposedParameter, duration, fromVolume, targetVolume, delay));
     }
 
     // Update is called once per frame
