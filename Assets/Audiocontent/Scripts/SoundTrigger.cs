41d0ba597d35a837fc4d6a0bdec5003411626c2b
Alexej <alexej.wieber@ultraschall-vr.com>
2020-11-08 18:39:36 +0100

    LoadingScreen Audio, Mushroom Pos, Optimized

---
+++ Assets/Audiocontent/Scripts/SoundTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioSource _Triggersource;
+   // [SerializeField] AudioClip _clip;
+    private bool isPlayed;
+
+    public void Awake() {
+        _Triggersource = GetComponent < AudioSource>();
+        isPlayed = false;
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("MainCamera") & !isPlayed) {
+            _Triggersource.Play();
+            isPlayed = true;
+        }
+    }
+
+}
