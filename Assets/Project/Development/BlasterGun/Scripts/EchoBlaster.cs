f95f345c0a25745ed57be973846394ede1adadf3
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2020-11-29 12:17:58 +0100

    Added Ammo System

--- Assets/Project/Development/BlasterGun/Scripts/EchoBlaster.cs
+++ Assets/Project/Development/BlasterGun/Scripts/EchoBlaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Echosystem.Resonance.Game
@@ -9,6 +10,7 @@ namespace Echosystem.Resonance.Game
         [SerializeField] private GameObject _blastPrefab;
         [SerializeField] private GameObject _overdrivePrefab;
         [SerializeField] private Transform _muzzleExplosion;
+        [SerializeField] private Light _muzzleLight;
 
         private bool _charging;
         private bool _charged;
@@ -16,9 +18,17 @@ namespace Echosystem.Resonance.Game
         private float _chargeThreshold = 0.2f;
         private float _maxCharge = 2.0f;
 
+        private float _ammo;
+        private float _maxAmmo;
+        private float _initialAmmo = 60;
+
         [SerializeField] private bool _debug;
-    
-    
+
+        private void Start()
+        {
+            AmmoUp();
+        }
+
         void Update()
         {
             if (_debug)
@@ -27,17 +37,29 @@ namespace Echosystem.Resonance.Game
             }
         }
 
+        public void AmmoUp()
+        {
+            _ammo = _initialAmmo;
+        }
+
         private void Debug()
         {
             if (Input.GetKeyUp(KeyCode.K))
             {
                 if (!_charged)
                 {
-                    SpawnBlast();
+                    if (_ammo >= 1f)
+                    {
+                        SpawnBlast();
+                    }
+                    
                 }
                 else
                 {
-                    SpawnOverdrive();
+                    if (_ammo >= 10f)
+                    {
+                        SpawnOverdrive();
+                    }
                 }
 
                 _charging = false;
@@ -65,12 +87,14 @@ namespace Echosystem.Resonance.Game
 
         private void SpawnOverdrive()
         {
+            _ammo = _ammo - 10;
             StartCoroutine(MuzzleExplosion(15));
             Instantiate(_overdrivePrefab, _tip.transform.position, _tip.transform.rotation);
         }
 
         private void SpawnBlast()
         {
+            _ammo = _ammo - 1;
             StartCoroutine(MuzzleExplosion(8));
             Instantiate(_blastPrefab, _tip.transform.position, _tip.transform.rotation);
         }
@@ -86,6 +110,7 @@ namespace Echosystem.Resonance.Game
 
                 float value = Mathf.Lerp(0, 1, t / timer);
                 
+                _muzzleLight.intensity = value * 10;
                 _muzzleExplosion.localScale = new Vector3(scale*value, scale*value, scale*value);
                 
                 yield return null;
@@ -100,6 +125,7 @@ namespace Echosystem.Resonance.Game
 
                 float value = Mathf.Lerp(1, 0, t / timer);
                 
+                _muzzleLight.intensity = value * 10;
                 _muzzleExplosion.localScale = new Vector3(scale*value, scale*value, scale*value);
                 
                 yield return null;
