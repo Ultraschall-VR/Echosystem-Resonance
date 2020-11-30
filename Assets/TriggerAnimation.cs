67c7a9c0f62cfc21e63fc6178157703e4ffdc062
db <demian.basse@ultraschall-vr.com>
2020-11-30 16:03:23 +0100

    StoneShroomProps Creation

---
+++ Assets/TriggerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Echosystem.Resonance.Game
+{
+    public class TriggerAnimation : MonoBehaviour
+    {
+        [SerializeField] private bool triggered;
+        Animator m_Animator;
+        
+        void Start()
+        {
+            m_Animator = gameObject.GetComponent<Animator>();
+            triggered = false;
+        }
+
+        void Update()
+        {
+            if (triggered == true)
+            {
+                m_Animator.SetBool("triggered", true);
+            }
+        }
+    }
+}
