669c6072ccaf246e2cce951e10143ac31f3f5c4c
DemianBasse <demian.basse@ultraschall-vr.com>
2020-12-02 18:25:35 +0100

    Quicksave -- Destroyable Object Script needs fixing

---
+++ Assets/Project/Development/Scripts/DestroyableObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Echosystem.Resonance.Game
+{
+    public class DestroyableObject : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private MeshRenderer _meshRenderer;
+
+        [SerializeField] private Collider _collider;
+        [SerializeField] private Collider _capCollider;
+
+        [SerializeField] private ParticleSystem _particle;
+        [SerializeField] private bool _particlesStopped;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _capCollider.enabled = false;
+        }
+
+        private void Update()
+        {
+            _speed = _rigidbody.velocity.magnitude;
+            _particlesStopped = false;
+            
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.relativeVelocity.magnitude > 2)
+            {
+                _meshRenderer.enabled = false;
+                _collider.enabled = false;
+                _capCollider.enabled = true;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_meshRenderer.enabled == false && !_particlesStopped)
+            {
+                StartCoroutine(PlayParticles());
+            }
+        }
+
+        private IEnumerator PlayParticles()
+        {
+            _particle.Play();
+            yield return new WaitForSeconds(2f);
+            
+            _particle.Stop();
+            yield return _particlesStopped = true;
+        }
+    }
+}
