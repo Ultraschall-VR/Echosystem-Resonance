704ad225651c5789b4f41199da53f06b62f9cd63
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-02-15 16:07:42 +0100

    Worked on it

---
+++ Assets/_Prototype/Loudness x Silence/Scripts/SceneSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SceneSettings : MonoBehaviour
+{
+    public float LoudnessIncreaseTime;
+    public float LoudnessDecreaseTime;
+
+    public static SceneSettings Instance;
+    
+    
+    private void Awake() 
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
