fad2c3ff3351e7bf94ec24831defe1f0a21b85f4
Alexej <alexej.wieber@ultraschall-vr.com>
2021-02-17 09:36:07 +0100

    Working Pause Menu

--- Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/FadeMixerGroupFromBack.cs
+++ Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/FadeMixerGroupFromBack.cs
@@ -6,15 +6,11 @@ using UnityEngine;
 public static class FadeMixerGroupFromBack
 {
 
-
     public static IEnumerator StartFadeFrom(AudioMixer audioMixer, string exposedParam, float duration, float fromVolume, float targetVolume) {
 
 
         float currentTime = 0;
-        /*  float currentVol;
-          audioMixer.GetFloat(exposedParam, out currentVol);
-          currentVol = Mathf.Pow(10, currentVol / 20);
-          */
+
         float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
 
         while (currentTime < duration) {
--- Assets/_Project/Core/Scripts/_GameScripts/Menu/PauseMenuEchosystem.cs
+++ Assets/_Project/Core/Scripts/_GameScripts/Menu/PauseMenuEchosystem.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Echosystem.Resonance.Prototyping;
-using UnityStandardAssets.Characters.FirstPerson;
 
 
 public class PauseMenuEchosystem : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
-
+    [SerializeField] private GameObject _pauseCamera;
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab)) {
-            if (GameIsPaused) {
+        Input();
+    }
+
+    private void Input()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameIsPaused)
+            {
                 ResumeToGame();
             }
-            else {
+            else
+            {
                 Pause();
             }
         }
@@ -29,7 +36,9 @@ public class PauseMenuEchosystem : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        Observer.Player.GetComponent<FirstPersonController>().enabled = true;
+       // Observer.Player.GetComponent<FirstPersonController>().enabled = true;
+       Observer.Player.SetActive(true);
+       _pauseCamera.SetActive(false);
         GameIsPaused = false;
 
     }
@@ -37,9 +46,12 @@ public class PauseMenuEchosystem : MonoBehaviour
     void Pause ()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        Cursor.visible = true;
-        Observer.Player.GetComponent<FirstPersonController>().enabled = false;
+        Time.timeScale = 0f; 
+     //   Observer.Player.GetComponent<FirstPersonController>().enabled = false;
+        Observer.Player.SetActive(false);
+        _pauseCamera.SetActive(true);
         GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
