e6501df8f2a4e38f5a3dcd3a45a4bf89c8afd090
Alexej <alexej.wieber@ultraschall-vr.com>
2021-02-17 10:53:56 +0100

    Working Menu

--- Assets/_Prototype/Menu/Scripts/LevelLoader.cs
+++ Assets/_Prototype/Menu/Scripts/LevelLoader.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class LevelLoader : MonoBehaviour
 {
+
     public Animator transition;
     public float transitionTime;
-    public AudioMixer audioMixer;
+    [SerializeField] private GameObject _pauseManager;
 
     public void LoadLevel(int sceneNumber)
     {
         Debug.Log("sceneBuildIndex to load:" + sceneNumber);
         StartCoroutine(LoadLevelTransition(sceneNumber));
-      //  StartCoroutine(FadeMixerGroupFromBack.StartFadeFrom(audioMixer, "masterVol", transitionTime, 1, 0));
-      //  StartCoroutine(FadeMixerGroupFrom.StartFadeFrom(audioMixer, "masterVol", transitionTime, 0, 1, 0));
     }
 
     IEnumerator LoadLevelTransition(int sceneNumber)
     {
+        if (PauseMenuEchosystem.GameIsPaused == true)
+        {
+            _pauseManager.GetComponent<PauseMenuEchosystem>().ResumeToGame();
+        }
+
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadScene(sceneNumber);
+        
     }
+   
 }
