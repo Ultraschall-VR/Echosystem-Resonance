ac655788c6c1db85e79c4b67b6b1bc2c72a0e82d
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-02-23 08:22:17 +0100

    Worked on UI oops

---
+++ Assets/_Prototype/Loudness x Silence/Scripts/ToggleUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ToggleUI : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvas;
+    
+    [SerializeField] private KeyCode _toggleKey;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(_toggleKey))
+        {
+            _canvas.enabled = !_canvas.enabled;
+        }
+    }
+}
