79e4ebcd607c69f31dde6cce7ff69203a62260f0
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-02-23 11:00:10 +0100

    Worked on Crosshair and Object Focus

---
+++ Assets/FocusedObject.cs
@@ -0,0 +1,33 @@
+﻿using Echosystem.Resonance.Prototyping;
+using TMPro;
+using UnityEngine;
+
+public class FocusedObject : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Observer.PlayerHead.transform.position, Observer.PlayerHead.transform.forward, out hit, 5f, ~LayerMask.GetMask("SilenceSphere")))
+        {
+            if (hit.collider.CompareTag("Player") || hit.collider.gameObject.isStatic)
+            {
+                _text.text = null;
+                return;
+            }
+            
+            _text.text = hit.collider.name;
+        }
+        else
+        {
+            _text.text = null;
+        }
+    }
+}
--- Assets/_Prototype/Loudness x Silence/Scripts/PitchShifter.cs
+++ Assets/_Prototype/Loudness x Silence/Scripts/PitchShifter.cs
@@ -1,20 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Echosystem.Resonance.Prototyping;
+﻿using Echosystem.Resonance.Prototyping;
 using UnityEngine;
 
 public class PitchShifter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = Observer.PlayerHead.transform.position;
         transform.eulerAngles = Observer.PlayerHead.transform.eulerAngles;
+        
+        
     }
 }
