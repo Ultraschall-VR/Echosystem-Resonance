d6d4bc52e916d674562f1cc96ff020908ac0cbb6
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-02-25 08:48:40 +0100

    WIP

---
+++ Assets/_Prototype/Loudness x Silence/Scripts/Pillar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(PitchShifterable))]
+public class Pillar : MonoBehaviour
+{
+    public float Pitch;
+    private Vector3 _position;
+    private Vector3 _initPos;
+    private AudioSource _audioSource;
+
+    private float _pitchMin = 0.33f;
+    private float _pitchMax = 1.33f;
+    
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        Pitch = Random.Range(_pitchMin, _pitchMax);
+        _initPos = transform.position;
+        _initPos.y += Pitch;
+        transform.position = _initPos;
+        _position = _initPos;
+    }
+
+    private void Update()
+    {
+        Pitch = Mathf.Clamp(Pitch, _pitchMin, _pitchMax);
+        var posY = _initPos.y + Pitch;
+        _audioSource.pitch = Pitch;
+        _position.y = posY;
+        transform.position = _position;
+    }
+}
--- Assets/_Prototype/Loudness x Silence/Scripts/PitchShifter.cs
+++ Assets/_Prototype/Loudness x Silence/Scripts/PitchShifter.cs
@@ -5,27 +5,68 @@ public class PitchShifter : MonoBehaviour
 {
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private Transform _tip;
-    
+
+    public bool _lockedTarget;
+    public GameObject _focusedObject;
+
     void Update()
+    {
+        HandleTransform();
+        HandleFocus();
+        
+        if (_focusedObject != null)
+        {
+            if (_focusedObject.GetComponent<Pillar>())
+            {
+                var pillar = _focusedObject.GetComponent<Pillar>();
+                
+                
+                
+            }
+        }
+    }
+
+    private void HandleTransform()
     {
         transform.position = Observer.PlayerHead.transform.position;
         transform.eulerAngles = Observer.PlayerHead.transform.eulerAngles;
+    }
 
+    private void HandleFocus()
+    {
         if (Observer.FocusedGameObject == null)
         {
             _lineRenderer.enabled = false;
+            DeactivateLock();
             return;
         }
         
-        if (Observer.FocusedGameObject.CompareTag("PitchShifterable"))
+        if (Observer.FocusedGameObject.GetComponent<PitchShifterable>())
         {
-            _lineRenderer.enabled = true;
-            _lineRenderer.SetPosition(0, _tip.position);
-            _lineRenderer.SetPosition(1, Observer.FocusedGameObject.transform.position);
+            if (!_lockedTarget)
+            {
+                var pitchShifterable = Observer.FocusedGameObject.GetComponent<PitchShifterable>();
+                
+                _lineRenderer.enabled = true;
+                _lineRenderer.SetPosition(0, _tip.position);
+                _lineRenderer.SetPosition(1, pitchShifterable.Grip.position);
+                
+                if (Input.GetMouseButton(0))
+                {
+                    _lineRenderer.enabled = false;
+                    _lockedTarget = true;
+                    _focusedObject = Observer.FocusedGameObject;
+                }
+            }
         }
-        else
+    }
+
+    private void DeactivateLock()
+    {
+        if (!Input.GetMouseButton(0))
         {
-            _lineRenderer.enabled = false;
+            _lockedTarget = false;
+            _focusedObject = null;
         }
     }
 }
