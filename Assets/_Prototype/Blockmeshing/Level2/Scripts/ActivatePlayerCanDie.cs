c0528a2e136bac7e7b3a2ca161e4740415c1c16b
Alexej <alexej.wieber@ultraschall-vr.com>
2021-03-10 13:22:04 +0100

    Quickfix Level 2

---
+++ Assets/_Prototype/Blockmeshing/Level2/Scripts/ActivatePlayerCanDie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Echosystem.Resonance.Prototyping;
+using UnityEngine;
+
+public class ActivatePlayerCanDie : MonoBehaviour
+{
+    private BeaconSocket _beaconSocket;
+
+    private bool _activated;
+
+    void Start()
+    {
+        _beaconSocket = GetComponent<BeaconSocket>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_beaconSocket.IsOccupied && !_activated)
+        {
+            _activated = true;
+            SceneSettings.Instance.PlayerCanDie = true;
+        }
+    }
+}
