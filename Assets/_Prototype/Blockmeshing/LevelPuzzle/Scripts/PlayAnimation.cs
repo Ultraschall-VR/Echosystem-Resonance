0e6c5314088923db0be46aa9ca8ad76f57934e74
DemianBasse <demian.basse@ultraschall-vr.com>
2021-03-10 12:28:38 +0100

    Created 'PlayAnimation' script & animated stairs

---
+++ Assets/_Prototype/Blockmeshing/LevelPuzzle/Scripts/PlayAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Echosystem.Resonance.Prototyping
+{
+    public class PlayAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _animator.SetBool("Condition", true);
+        }
+    }
+}
