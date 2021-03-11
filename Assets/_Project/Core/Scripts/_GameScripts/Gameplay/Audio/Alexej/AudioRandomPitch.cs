38eef106b2096d30b46f1ffebcf549441fb65e9b
Alexej <alexej.wieber@ultraschall-vr.com>
2021-03-11 17:38:01 +0100

    Quickfix

--- Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/AudioRandomPitch.cs
+++ Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/AudioRandomPitch.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class AudioRandomPitch : MonoBehaviour
+namespace Echosystem.Resonance.Prototyping
 {
-    AudioSource _audioSource;
-    [SerializeField] float pitchMin = 1, pitchMax = 1, volumeMin = 1, volumeMax = 1;
-
-    // Start is called before the first frame update
-    void Start() {
-        _audioSource = GetComponent<AudioSource>();
-        _audioSource.pitch = Random.Range(pitchMin, pitchMax);
-        _audioSource.volume = Random.Range(volumeMin, volumeMax);
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class AudioRandomPitch : MonoBehaviour
     {
-        
+        AudioSource _audioSource;
+        [SerializeField] float pitchMin = 1, pitchMax = 1, volumeMin = 1, volumeMax = 1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.pitch = Random.Range(pitchMin, pitchMax);
+            _audioSource.volume = Random.Range(volumeMin, volumeMax);
+        }
     }
 }
---
+++ Assets/_Prototype/Blockmeshing/LevelPuzzle/Scripts/PlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Echosystem.Resonance.Prototyping
+{
+    public class PlayAudio : MonoBehaviour
+    {
+        private List <AudioSource> _audioSources;
+
+        private void Start()
+        {
+            _audioSources = GetComponentInChildren<List<AudioSource>>();
+            foreach (var audio in _audioSources)
+            {
+                audio.Play();
+            }
+        }
+    }
+}
