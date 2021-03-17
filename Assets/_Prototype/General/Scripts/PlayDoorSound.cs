69ecea65e59f47ee1a296d428c99e4321835b925
AlexejWieber <alexej.wieber@ultraschall-vr.com>
2021-03-17 19:20:58 +0100

    Included Door Sound everywhere, Included Particle System...

---
+++ Assets/_Prototype/General/Scripts/PlayDoorSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Echosystem.Resonance.Prototyping
+{
+    public class PlayDoorSound : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [Range(1, 4)] [SerializeField] private int _playAfterMelody;
+
+        private bool _activated;
+
+        void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+            if (CollectibleManager.Index == _playAfterMelody && !_activated)
+            {
+                _activated = true;
+                _audioSource.Play();
+            }
+        }
+    }
+}
