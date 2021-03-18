f9f32b870c3dc327e52054d337a130deda98a6bf
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-03-18 12:21:02 +0100

    Quicksave

---
+++ Assets/ScaleByDistance.cs
@@ -0,0 +1,20 @@
+﻿using Echosystem.Resonance.Prototyping;
+using UnityEngine;
+
+public class ScaleByDistance : MonoBehaviour
+{
+    private Vector3 _initPos;
+    void Start()
+    {
+        _initPos = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Observer.Player.transform.position);
+        transform.LookAt(Observer.PlayerHead.transform.position);
+        
+        transform.localScale = _initPos * distance;
+    }
+}
