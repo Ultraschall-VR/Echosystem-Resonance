372df088e43693c77f738f34479ba2ed0f2549ee
AlexejWieber <alexej.wieber@ultraschall-vr.com>
2021-03-25 19:10:32 +0100

    Level Checker

---
+++ Assets/_Prototype/General/Scripts/LevelChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChecker : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int levelUnlocked = PlayerPrefs.GetInt("levelUnlocked", 1);
+        Debug.Log("Unlocked Levels:" + PlayerPrefs.GetInt("levelUnlocked"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
