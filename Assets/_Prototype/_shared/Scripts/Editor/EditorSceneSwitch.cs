f052ec0fcd47bc589cc5be966780de3e83347968
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-03-29 22:05:47 +0200

    Refactored Project.

---
+++ Assets/_Prototype/_shared/Scripts/Editor/EditorSceneSwitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class EditorSceneSwitch
+{
+    [MenuItem ("Scenes/LevelMenu")]
+    public static void OpenLevelMenu()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/_Prototype/Scenes/LevelMenu.unity");
+    }
+    
+    [MenuItem ("Scenes/Level0")]
+    public static void OpenLevel0()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/_Prototype/Scenes/Level0.unity");
+    }
+    
+    [MenuItem ("Scenes/Level1")]
+    public static void OpenLevel1()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/_Prototype/Scenes/Level1.unity");
+    }
+    
+    [MenuItem ("Scenes/Level2")]
+    public static void OpenLevel2()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/_Prototype/Scenes/Level2.unity");
+    }
+    
+    [MenuItem ("Scenes/Level3")]
+    public static void OpenLevel3()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/_Prototype/Scenes/Level3.unity");
+    }
+}
