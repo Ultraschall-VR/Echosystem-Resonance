bc359d4ce99ce8b09797128b0ce89f13f0b0836b
DemianBasse <demian.basse@ultraschall-vr.com>
2021-03-30 17:45:53 +0200

    Some shader stuff

---
+++ Assets/_Prototype/LevelCinematic/Water/UnderwaterEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class UnderwaterEffect : MonoBehaviour
+{
+    public Material _mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, _mat);
+    }
+}
