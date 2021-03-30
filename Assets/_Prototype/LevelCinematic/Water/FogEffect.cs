1e89f594b5f71dd7129e6a82f2f037702fa5ed34
DemianBasse <demian.basse@ultraschall-vr.com>
2021-03-30 18:41:29 +0200

    Some more shader stuff for underwater. does not seem to work tho

---
+++ Assets/_Prototype/LevelCinematic/Water/FogEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class FogEffect : MonoBehaviour
+{
+    public Material _Material;
+    public Color _FogColor;
+    public float _DepthStart;
+    public float _DepthDistance;
+
+    private void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    private void Update()
+    {
+        _Material.SetColor("_FogColor", _FogColor);
+        _Material.SetFloat("_DepthStart", _DepthStart);
+        _Material.SetFloat("_DepthDistance", _DepthDistance);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, _Material);
+    }
+}
--- Assets/_Prototype/LevelCinematic/Water/UnderwaterEffect.cs
+++ Assets/_Prototype/LevelCinematic/Water/UnderwaterEffect.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class UnderwaterEffect : MonoBehaviour
 {
-    public Material _mat;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Material _Material;
+    [Range(0.001f, 0.1f)] public float _PixelOffset;
+    [Range(0.1f, 20f)] public float _NoiseScale;
+    [Range(0.1f, 20f)] public float _NoiseFrequency;
+    [Range(0.1f, 30f)] public float _NoiseSpeed;
+    public float _DepthStart;
+    public float _DepthDistance;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _Material.SetFloat("_NoiseFrequency", _NoiseFrequency);
+        _Material.SetFloat("_NoiseSpeed", _NoiseSpeed);
+        _Material.SetFloat("_NoiseScale", _NoiseScale);
+        _Material.SetFloat("_PixelOffset", _PixelOffset);
+        _Material.SetFloat("_DepthStart", _DepthStart);
+        _Material.SetFloat("_DepthDistance", _DepthDistance);
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        Graphics.Blit(src, dest, _mat);
+        Graphics.Blit(src, dest, _Material);
     }
 }
