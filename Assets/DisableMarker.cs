09d5b7be5040ef8b51492342911c506da8a2a803
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-04-01 15:36:03 +0200

    UI done

---
+++ Assets/DisableMarker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DisableMarker : MonoBehaviour
+{
+    [SerializeField] private GameObject _marker;
+    
+    void Start()
+    {
+        _marker.SetActive(false);
+    }
+}
