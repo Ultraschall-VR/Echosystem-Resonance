eb4ebaf7049e65a5010bef998b73bf9fc0d4591a
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-04-06 16:07:47 +0200

    PauseMenu implemented

---
+++ Assets/Continue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Continue : MonoBehaviour
+{
+    private PauseMenu _pauseMenu;
+    void OnEnable()
+    {
+        _pauseMenu = FindObjectOfType<PauseMenu>();
+        _pauseMenu.Toggle();
+    }
+}
