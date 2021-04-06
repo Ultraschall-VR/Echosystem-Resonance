b4832556fe3858b4192e58727376eaddeacf2f0d
AlexejWieber <alexej.wieber@ultraschall-vr.com>
2021-04-06 21:10:36 +0200

    Spatial Audio LevelMenu

---
+++ Assets/_Prototype/LevelMenu/Scripts/ActivateRing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRing : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    [SerializeField] private GameObject _ring; 
+    private bool _activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _ring.gameObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  if (!_activated && ) 
+      //  {
+      //      _activated = true;
+      //      _ring.gameObject.SetActive(true);
+      //  }
+      //  
+    }
+}
