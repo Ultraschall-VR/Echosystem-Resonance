d34ce09d77c976e984527d26a45cb6b9d7713971
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-04-07 09:56:10 +0200

    Added CinemaMode

---
+++ Assets/_Prototype/_shared/Scripts/CinemaModePostProcessing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CinemaModePostProcessing : MonoBehaviour
+{
+    private Volume _postProcessing;
+
+    [SerializeField] private VolumeProfile _cinemaVolumeProfile;
+    [SerializeField] private VolumeProfile _defaultProfile;
+    void Start()
+    {
+        _postProcessing = GetComponent<Volume>();
+
+        if (SceneSettings.Instance.CinemaMode)
+            _postProcessing.profile = _cinemaVolumeProfile;
+        else
+            _postProcessing.profile = _defaultProfile;
+    }
+}
