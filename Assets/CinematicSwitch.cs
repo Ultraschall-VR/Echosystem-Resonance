5da48ceacb0130f377ae616617d809054938d9e1
Albert Oelschlägel <albert.oelschlaegel@ultraschall-vr.com>
2021-04-08 15:26:03 +0200

    WIP

---
+++ Assets/CinematicSwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CinematicSwitch : MonoBehaviour
+{
+    [SerializeField] private bool _isVR;
+
+    private void Start()
+    {
+        if (SceneSettings.Instance.VREnabled)
+        {
+            if(_isVR)
+                gameObject.SetActive(true);
+            else
+                gameObject.SetActive(false);
+        }
+        else
+        {
+            if(_isVR)
+                gameObject.SetActive(false);
+            else
+                gameObject.SetActive(true);
+        }
+    }
+}
