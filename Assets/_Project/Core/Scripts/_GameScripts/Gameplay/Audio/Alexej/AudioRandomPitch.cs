22b83e1f313c970c683bfd4362632b603b1d025c
AlexejWieber <alexej.wieber@ultraschall-vr.com>
2021-04-19 19:02:29 +0200

    Cinematic Mix Change,Doorsound issue Fix, Debug RoomAnalyzer DearVR

--- Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/AudioRandomPitch.cs
+++ Assets/_Project/Core/Scripts/_GameScripts/Gameplay/Audio/Alexej/AudioRandomPitch.cs
@@ -5,14 +5,17 @@ namespace Echosystem.Resonance.Prototyping
     public class AudioRandomPitch : MonoBehaviour
     {
         AudioSource _audioSource;
+        [SerializeField] private bool _randomizeVolume;
         [SerializeField] float pitchMin = 1, pitchMax = 1, volumeMin = 1, volumeMax = 1;
 
         // Start is called before the first frame update
         void Start()
         {
             _audioSource = GetComponent<AudioSource>();
+            
             _audioSource.pitch = Random.Range(pitchMin, pitchMax);
-            _audioSource.volume = Random.Range(volumeMin, volumeMax);
+            if (_randomizeVolume)
+              _audioSource.volume = Random.Range(volumeMin, volumeMax);
         }
     }
 }
